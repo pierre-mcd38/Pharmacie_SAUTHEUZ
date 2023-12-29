1471c0c52b54cfd9f7798e4f56384a3aecfde2a9
Pierre <pierre.Michaud3806@gmail.com>
2023-12-29 11:27:01 +0100

    résolution bug affichage

--- routes/medecinsRoute.js
+++ routes/medecinsRoute.js
@@ -3,8 +3,8 @@ const routeur = express.Router();
 const ctrlMedecin = require('../controllers/medecinsController.js');
 
 
-routeur.get('/medecins/afficher', ctrlMedecin.controlMed.afficherMedecin)
-routeur.post('/medecins/ajouter', ctrlMedecin.controlMed.ajouterMedecin)
+routeur.get('/medecins/afficher', ctrlMedecin.controlMed.afficherMedecin);
+routeur.post('/medecins/ajouter', ctrlMedecin.controlMed.ajouterMedecin);
 routeur.post('/medecins/supprimer/:id', ctrlMedecin.controlMed.supprimerMedecin);
 routeur.get('/medecins/modifier/:id', ctrlMedecin.controlMed.afficherUnMedecin);
 routeur.post('/medecins/modifier', ctrlMedecin.controlMed.modifierMedecin);
